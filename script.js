4880e4935ee2cbf40afc60bcb8864e3a52eb39fc
xRaquelx <99687457+xRaquelx@users.noreply.github.com>
2024-03-22 14:39:13 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-//Loading screen
-window.addEventListener("load", ()=> {
-    const loading = document.querySelector(".loading-screen");
-    setTimeout(event => loading.classList.add("loading-screen_hidden"), "1000");
-    loading.addEventListener("transitionend", event => document.body.removeChild(loading));
-});
